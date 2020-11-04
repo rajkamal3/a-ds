3ee344e37f240cb83a0a10824bf320636b829838
Raj Kamal Chenumalla <rkchenumalla@gmail.com>
2020-11-04 23:11:31 +0530

    short sum

--- src/2-2-addUpto.js
+++ src/2-2-addUpto.js
@@ -15,7 +15,7 @@ function sum(num) {
 }
 
 // let t1 = performance.now();
-// let ans = sum(10000000000);
+// let ans = sum(1000000000);
 // console.log(ans);
 // let t2 = performance.now();
 // console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds`);
