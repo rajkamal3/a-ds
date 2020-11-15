2b98549212cd11f82abb1c4018ee9874226f6af4
rajkamal3 <rkchenumalla@gmail.com>
2020-11-15 23:21:58 +0530

    4-6

---
+++ src/code/4-6-charCount.js
@@ -0,0 +1,23 @@
+function charCount(str) {
+    // make object to return at end
+    var result = {};
+
+    // loop over string, for each character
+    for (var i = 0; i < str.length; i++) {
+        var char = str[i].toLowerCase();
+        // if the char is a number/letter AND is a key in object, add one to count
+        if (result[char] > 0) {
+            result[char]++;
+        }
+        // if the char is a number/letter AND not in object, add one it to object and set value to 1
+        else {
+            result[char] = 1;
+        }
+    }
+    // if character is something else (space, period, etc.) don't do anything
+
+    // return object at end
+    return result;
+}
+
+console.log(charCount('Hi there!!!'));
