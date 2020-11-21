aaf993032d77173a8e39d2d7c6d98ff77fc145ab
Raj Kamal Chenumalla <rkchenumalla@gmail.com>
2020-11-21 20:27:41 +0530

    Update sumZero.js

--- src/programs/sumZero.js
+++ src/programs/sumZero.js
@@ -1,9 +1,29 @@
+// Time complexity: O(N^2)
+// Space complexity: O(1)
 function sumZero(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[i] + arr[j] === 0) {
                 return [arr[i], arr[j]]
             }
+        }
+    }
+}
+
+// Time complexity: O(N)
+// Space complexity: O(1)
+function sumZero(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+        if (sum === 0) {
+            return [arr[left], arr[right]]
+        } else if (sum > 0) {
+            right--;
+        } else {
+            left++
         }
     }
 }
