af5a946d49cb2da36cace267d146f658cf82079a
Raj Kamal Chenumalla <rkchenumalla@gmail.com>
2020-11-22 23:40:30 +0530

    Update and rename 7-11-helperMethodRecursion.js to 7-11-collectOddValues.js

--- src/programs/7-11-collectOddValues.js
+++ src/programs/7-11-collectOddValues.js
@@ -1,3 +1,4 @@
+// Using helper method
 function collectOddValues(arr) {
     let result = [];
 
@@ -20,3 +21,19 @@ function collectOddValues(arr) {
 
 collectOddValues([1, 2, 3, 4, 5, 6, 7, 8]);
 
+// My solution
+function collectOddValues(arr) {
+    if (arr.length === 0) {
+        return;
+    }
+
+    let odds = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            odds.push(arr[i]);
+        }
+    }
+
+    return odds;
+}
